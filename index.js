13c33aa81c40e5fab716fe2839b33442a6dab692
Olegzandr Denman <alex@oresoftware.com>
2017-06-17 21:41:53 -0700

    pdev:set

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var residence = require("residence");
 var util = require("util");
+var cp = require("child_process");
 var root = residence.findProjectRoot(process.cwd());
 var fs = require('fs');
 var async = require('async');
@@ -19,9 +20,16 @@ var isMatch = function (pth) {
         return String(pth).match(ign);
     });
 };
+var logsDir = path.resolve(root + '/.tscmwlogs');
+try {
+    fs.mkdirSync(logsDir);
+}
+catch (err) {
+}
 var tsconfigPaths = [];
 var searchDir = function (dir, cb) {
     if (isMatch(dir)) {
+        console.log('dir was ignored => ', dir);
         return process.nextTick(cb);
     }
     fs.readdir(dir, function (err, items) {
@@ -42,15 +50,55 @@ var searchDir = function (dir, cb) {
                         tsconfigPaths.push(fullPath);
                     }
                 }
+                cb(null);
             });
-        });
+        }, cb);
     });
 };
 searchDir(root, function (err) {
     if (err) {
         throw err;
     }
+    if (tsconfigPaths.length < 1) {
+        console.error('No tsconfig.json files could be found in your project.');
+        return process.exit(1);
+    }
+    else {
+        console.log(" => tsc-multi-watch will start " + tsconfigPaths.length + " watching processes.");
+    }
     async.eachLimit(tsconfigPaths, 3, function (p, cb) {
+        var logFile = path.resolve(root + '/.tscmwlogs/' + String(p).slice(root.length).replace('/', '#') + '.log');
+        var callable = true;
+        var first = function () {
+            if (callable) {
+                clearTimeout(to);
+                k.stderr.removeListener('data', onStdio);
+                k.stdout.removeListener('data', onStdio);
+                callable = false;
+                cb.apply(this, arguments);
+            }
+        };
+        var to = setTimeout(first, 8000);
+        var k = cp.spawn('bash', [], {
+            detached: false,
+            cwd: path.dirname(p)
+        });
+        var cmd = 'tsc -w';
+        k.stdin.write("\n" + cmd + "\n");
+        k.once('error', first);
+        k.stderr.setEncoding('utf8');
+        k.stdout.setEncoding('utf8');
+        var count = 0;
+        var onStdio = function () {
+            if (count++ > 15) {
+                first();
+            }
+        };
+        var strm = fs.createWriteStream(logFile);
+        k.stdout.pipe(strm);
+        k.stderr.pipe(strm);
+        k.stdout.on('data', onStdio);
+        k.stderr.on('data', onStdio);
     }, function (err) {
         if (err) {
             throw err;
--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@
 import * as residence from 'residence';
 import {Stats} from "fs";
 import * as util from "util";
+import * as cp from 'child_process';
 const root = residence.findProjectRoot(process.cwd());
 const fs = require('fs');
 const async = require('async');
@@ -12,25 +13,38 @@ if (!root) {
   throw new Error('=> Could not find an NPM project root given your current working directory.');
 }
 
-const ignored : Array<RegExp> = [
+const ignored: Array<RegExp> = [
   /\/node_modules\//,
   /\/.git\//,
   /\/bower_components\//
 ];
 
-let isMatch = function(pth: string): boolean{
-  return ignored.some(function(ign){
-     return String(pth).match(ign);
+let isMatch = function (pth: string): boolean {
+  return ignored.some(function (ign) {
+    return String(pth).match(ign);
   });
 };
+
+
+const logsDir = path.resolve(root + '/.tscmwlogs');
+
+try {
+  fs.mkdirSync(logsDir);
+}
+catch (err) {
+
+
+}
+
 
 const tsconfigPaths: Array<string> = [];
 
 
 let searchDir = function (dir: string, cb: Function) {
 
-  if(isMatch(dir)){
+  if (isMatch(dir)) {
     // we ignore paths that match any of the regexes in the list
+    console.log('dir was ignored => ', dir);
     return process.nextTick(cb);
   }
 
@@ -60,11 +74,13 @@ let searchDir = function (dir: string, cb: Function) {
           }
         }
 
+        cb(null);
+
 
       });
 
 
-    });
+    }, cb);
 
 
   });
@@ -79,17 +95,69 @@ searchDir(root, function (err: Error) {
     throw err;
   }
 
-  async.eachLimit(tsconfigPaths, 3, function(p: string, cb: Function){
+  if (tsconfigPaths.length < 1) {
+    console.error('No tsconfig.json files could be found in your project.');
+    return process.exit(1);
+  }
+  else {
+    console.log(` => tsc-multi-watch will start ${tsconfigPaths.length} watching processes.`);
+  }
+
+  async.eachLimit(tsconfigPaths, 3, function (p: string, cb: Function) {
 
 
-  }, function(err){
+    let logFile = path.resolve(root + '/.tscmwlogs/' + String(p).slice(root.length).replace('/', '#') + '.log');
 
-    if(err){
+    let callable = true;
+
+    let first = function () {
+      if (callable) {
+        clearTimeout(to);
+        k.stderr.removeListener('data', onStdio);
+        k.stdout.removeListener('data', onStdio);
+        callable = false;
+        cb.apply(this, arguments);
+      }
+    };
+
+    let to = setTimeout(first, 8000);
+
+    let k = cp.spawn('bash', [], {
+      detached: false,
+      cwd: path.dirname(p)
+    });
+
+    let cmd = 'tsc -w';
+    k.stdin.write(`\n${cmd}\n`);
+    k.once('error', first);
+    k.stderr.setEncoding('utf8');
+    k.stdout.setEncoding('utf8');
+
+
+    let count = 0;
+
+    let onStdio = function () {
+      if (count++ > 15) {
+        first();
+      }
+    };
+
+    let strm = fs.createWriteStream(logFile);
+
+    k.stdout.pipe(strm);
+    k.stderr.pipe(strm);
+    k.stdout.on('data', onStdio);
+    k.stderr.on('data', onStdio);
+
+  }, function (err: Error) {
+
+    if (err) {
       throw err;
     }
 
     console.log(' => tsc-multi-watch is running watchers against the following paths:');
     console.log(util.inspect(tsconfigPaths));
+
   });
 
 
